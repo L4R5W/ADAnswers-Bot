7c9b0640897c2ff7f3cf9214a27e73625c949b5f
L4R5W <66326188+L4R5W@users.noreply.github.com>
2021-04-23 15:16:08 +0200

    Changed numbers to their actual value
    Hellbach was to lazy, so I did it instead.
    I really like to help with these things and support you and your great work.

--- commands/grindingforbreak.js
+++ commands/grindingforbreak.js
@@ -6,6 +6,6 @@ module.exports = {
   name: "grindingforbreak",
   description: "Describes how to reach Break Infinity.",
   execute(message) {
-    message.channel.send(`Get the 10,000 cost 2x IP then save up 30,000 (60,000 on web) IP and max your crunch autobuyer interval (you'll need to complete C12 if you haven't already)`);
+    message.channel.send(`Get the 10,000 cost 2x IP then save up 32,767 IP (2^15 - 1) on mobile and 65,535 IP (2^16 - 1) on web and max your crunch autobuyer interval (you'll need to complete C12 if you haven't already)`);
   }
 };
